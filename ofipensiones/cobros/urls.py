ba8c345f48c17ffcac07e914e64ed2fa0f8706d6
Miguel Angel Ariza Jimenez <m.arizaj@uniandes.edu.co>
2024-10-08 23:22:56 +0000

    health check urls.py

--- ofipensiones/cobros/urls.py
+++ ofipensiones/cobros/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.get_cobros, name='get_cobros'),
-    path('<int:pk>', views.get_cobro, name='get_cobro')
+    path('<int:pk>', views.get_cobro, name='get_cobro'),
+    path('health-check/', views.healthCheck)
 ]
